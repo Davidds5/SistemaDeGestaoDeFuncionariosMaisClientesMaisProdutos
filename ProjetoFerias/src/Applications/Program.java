b76cf117e2c99e23ddf0a4c4887629190a9732bc
Clovin <fudeoagr12@gmail.com>
2025-07-22 13:42:35 -0300

    Comecando a fazer o main
    comecando a fazer menu com o do, o usuario escolhera oq fazer, qual gestao mecher

---
+++ ProjetoFerias/src/Applications/Program.java
@@ -0,0 +1,18 @@
+package Applications;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int op;
+
+        do{
+            System.out.println("=== Menu Principal ===");
+            System.out.println("1 - Gestao de Funcionarios");
+            System.out.println("2 - Gestao de Clintes");
+            System.out.println();
+            System.out.println();
+        }
+    }
+}
--- ProjetoFerias/src/Produtos/ProdutoUsado.java
+++ ProjetoFerias/src/Produtos/ProdutoUsado.java
@@ -5,12 +5,18 @@ import java.util.Date;
 public class ProdutoUsado extends Produtos {
     private Date dataFabricacao;
 
-    protected ProdutoUsado(String nome, Double preco) {
+    protected ProdutoUsado(String nome, Double preco, Date dataFabricacao) {
         super(nome, preco);
+        this.dataFabricacao = dataFabricacao;
     }
 
     @Override
     public Double precoEtiqueta() {
-        return 0.0;
+        return preco * 0.08;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "(usado, fabriaco em: " + dataFabricacao + ")";
     }
 }
