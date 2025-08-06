63dacb085027e2ca6036f51df582d6ccd67d5c5f
Clovin <fudeoagr12@gmail.com>
2025-08-06 11:16:18 -0300

    Finalizando projeto

--- ProjetoFerias/src/Applications/Program.java
+++ ProjetoFerias/src/Applications/Program.java
@@ -7,7 +7,6 @@ import Produtos.Produtos;
 import Usuario.Usuario;
 
 import java.util.*;
-import java.util.spi.AbstractResourceBundleProvider;
 
 public class Program {
     public static void main(String[] args) {
@@ -20,170 +19,169 @@ public class Program {
 
         int op;
 
-        do{
+        do {
             System.out.println("=== Menu Principal ===");
-            System.out.println("1 - Gestao de Funcionarios");
-            System.out.println("2 - Gestao de Clientes");
-            System.out.println("3 - Gestao de Produtos");
+            System.out.println("1 - Gestão de Funcionários");
+            System.out.println("2 - Gestão de Clientes");
+            System.out.println("3 - Gestão de Produtos");
             System.out.println("0 - Sair");
-            System.out.println("Digite a opcao desejada: ");
+            System.out.print("Digite a opção desejada: ");
             op = sc.nextInt();
+            sc.nextLine(); // Limpa buffer
 
-            System.out.println("__________________________________________________________________");
-            switch (op){
+            System.out.println("________________________________________________");
 
-                // usando o metodo do, eu farei o menu roda ate ser digitado o numero 0
-                // isso vale para todas as partes
-
+            switch (op) {
                 case 1:
-                    int opFuncionario;
-                  do {
-                      System.out.println("=== Menu funcionario ===");
-                      System.out.println("1 - Cadastro de Funcionario");
-                      System.out.println("2 - Lista de Funcionario");
-                      System.out.println("3 - Buscar Funcionario");
-                      System.out.println("4 - Remover Funcionario");
-                      System.out.println("0 - Sair");
-                      opFuncionario = sc.nextInt();
-                      System.out.println("------------------------------------------------------------------------------");
-
-                      switch (opFuncionario){
-                          // parte usada para o desenvolvimento do cadastro do funcionario, como pode ver iremos inserir os dados
-                          // depois armazena nunca lista ja extendida da class Funcionario
-                          // as listas serao diferente dependo da class de funcionario que o usuario inserir, clt ou pj
-                          // decidir um metodo que aprendi atraves de estudos para deixar o main mais organizado
-                          case 1:
-                              // so os dados do static ultilizado fora do main
-                              cadrastaFuncionario(sc, funcionarioList);
-                              break;
-                          case 2:
-                              listaFuncionario(funcionarioList);
-                              break;
-                          case 3:
-                              buscaFuncionario(sc, funcionarioList);
-                              break;
-                          case 4:
-                              removerFuncionario(sc, funcionarioList);
-                              break;
-                          case 0:
-                              System.out.println("Programa sendo encerrado...");
-                              break;
-                          default:
-                              System.out.println("Opcao invalida, tente novamente.");
-                      }
-                  }while(opFuncionario != 0);
-
-                  break;
-
-
+                    menuFuncionarios(sc, funcionarioList);
+                    break;
                 case 2:
-                    int opCliente;
-                    do {
-                        System.out.println();
-                        System.out.println();
-                        System.out.println();
-                        System.out.println();
-                        System.out.println();
-                        System.out.println();
-                        opCliente = sc.nextInt();
-                    }while (opCliente != 0);
-
+                    System.out.println("Módulo de clientes ainda não implementado.");
+                    break;
                 case 3:
+                    System.out.println("Módulo de produtos ainda não implementado.");
+                    break;
                 case 0:
-
+                    System.out.println("Encerrando o programa...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
             }
 
+        } while (op != 0);
 
-
-
-        }while(op != 0);
         sc.close();
-
     }
 
-    // PARTE DA CLASS FUNCIONARIO
-    // metodo apenas usado para o cadrasto de funcionario
-    public static void cadrastaFuncionario(Scanner sc, List<Funcionario> funcionarioList){
-        System.out.println("=== CADASTRADO DE FUNCIONARIO ===");
+    // Menu de funcionários
+    public static void menuFuncionarios(Scanner sc, List<Funcionario> funcionarioList) {
+        int opFuncionario;
 
-        System.out.println("Quantos funcionarios deseja cadastrado: ");
+        do {
+            System.out.println("=== Menu Funcionários ===");
+            System.out.println("1 - Cadastrar Funcionário");
+            System.out.println("2 - Listar Funcionários");
+            System.out.println("3 - Buscar Funcionário");
+            System.out.println("4 - Remover Funcionário");
+            System.out.println("0 - Voltar");
+            System.out.print("Digite a opção desejada: ");
+            opFuncionario = sc.nextInt();
+            sc.nextLine(); // Limpa buffer
+            System.out.println("------------------------------------------------");
+
+            switch (opFuncionario) {
+                case 1:
+                    cadastraFuncionario(sc, funcionarioList);
+                    break;
+                case 2:
+                    listaFuncionario(funcionarioList);
+                    break;
+                case 3:
+                    buscaFuncionario(sc, funcionarioList);
+                    break;
+                case 4:
+                    removerFuncionario(sc, funcionarioList);
+                    break;
+                case 0:
+                    System.out.println("Voltando ao menu principal...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+
+        } while (opFuncionario != 0);
+    }
+
+    // Cadastrar funcionário
+    public static void cadastraFuncionario(Scanner sc, List<Funcionario> funcionarioList) {
+        System.out.println("=== Cadastro de Funcionário ===");
+        System.out.print("Quantos funcionários deseja cadastrar? ");
         int N = sc.nextInt();
         sc.nextLine();
 
-        for (int i = 0; i <N ; i++) {
-            System.out.println("Funcionario #" + (i + 1) + ": ");
-            System.out.println("CLT ou PJ (C/P): ");
+        for (int i = 0; i < N; i++) {
+            System.out.println("Funcionário #" + (i + 1) + ":");
+            System.out.print("CLT ou PJ (C/P): ");
             char type = sc.next().toLowerCase().charAt(0);
             sc.nextLine();
 
-            System.out.println("Nome: ");
+            System.out.print("Nome: ");
             String nome = sc.nextLine();
 
-            System.out.println("Salario Base: ");
-            Double salarioBase = sc.nextDouble();
+            System.out.print("Salário Base: ");
+            double salarioBase = sc.nextDouble();
 
-            if (type == 'c'){
-                System.out.println("Beneficios: ");
+            if (type == 'c') {
+                System.out.print("Benefícios: ");
                 double beneficios = sc.nextDouble();
-
-                funcionarioList.add(new CLT(nome, salarioBase,beneficios));
-            }else if (type == 'p'){
+                funcionarioList.add(new CLT(nome, salarioBase, beneficios));
+            } else if (type == 'p') {
                 System.out.print("Horas trabalhadas: ");
                 int horas = sc.nextInt();
+                System.out.print("Valor por hora: ");
+                double valorHora = sc.nextDouble();
+                funcionarioList.add(new PJ(nome, salarioBase, horas, valorHora));
+            } else {
+                System.out.println("Tipo inválido! Funcionário não cadastrado.");
+            }
 
-                System.out.println("Valor por horas: ");
-                double valorHoras = sc.nextDouble();
+            sc.nextLine(); // Limpa buffer
+        }
+    }
 
-                funcionarioList.add(new PJ(nome, salarioBase, horas, valorHoras));
-        }else {
-        System.out.println("Tipo Invalido !");
+    // Listar funcionários
+    public static void listaFuncionario(List<Funcionario> funcionarioList) {
+        System.out.println("=== Lista de Funcionários ===");
+        if (funcionarioList.isEmpty()) {
+            System.out.println("Nenhum funcionário cadastrado.");
+        } else {
+            for (Funcionario funcionario : funcionarioList) {
+                System.out.println(funcionario);
             }
         }
     }
-    // metodo apenas usado para lista funcionarios
-    public static void listaFuncionario(List<Funcionario> funcionarioList) {
-        System.out.println("Listando Funcionarios");
-        for (Funcionario funcionario : funcionarioList){
-            System.out.println(funcionario);
-        }
-    }
 
-    // metodo apenas usado para encotrar funcionario
-    public static void buscaFuncionario(Scanner sc, List<Funcionario> funcionarioList){
-        System.out.println("=== Busca por Funcionarios ===");
-        System.out.print("Informe o nome do funcionario que deseja buscar: ");
+    // Buscar funcionário
+    public static void buscaFuncionario(Scanner sc, List<Funcionario> funcionarioList) {
+        System.out.println("=== Busca por Funcionário ===");
+        System.out.print("Informe o nome do funcionário: ");
         String nomeBusca = sc.nextLine();
-        boolean encotrado = false;
-        for (Funcionario funcionario : funcionarioList){
-            if (funcionario.getNome().equalsIgnoreCase(nomeBusca)){
-                encotrado = true;
-                System.out.println("Funcionario encontrado com sucesso.");
+        boolean encontrado = false;
+
+        for (Funcionario funcionario : funcionarioList) {
+            if (funcionario.getNome().equalsIgnoreCase(nomeBusca)) {
+                System.out.println("Funcionário encontrado:");
                 System.out.println(funcionario);
-
-        }
-    }
-    if (!encotrado){
-        System.out.println("Funcionario nao encotrado.");
-    }
-    }
-    // metodo usado apenas para remover funcionario
-    public static void removerFuncionario(Scanner sc, List<Funcionario> funcionarioList){
-        System.out.println("=== Remover Funcionario ===");
-        System.out.print("Qual funcionario deseja remover: ");
-        String nomeRemover = sc.nextLine();
-        boolean encotrado = false;
-
-        Iterator<Funcionario> iterator = funcionarioList.iterator();
-        for (Funcionario funcionario : funcionarioList){
-            if (funcionario.getNome().equalsIgnoreCase(nomeRemover)){
-                iterator.remove();
+                encontrado = true;
                 break;
             }
         }
-        if (!encotrado){
-            System.out.println("Nome nao encotrado.");
-        }
 
+        if (!encontrado) {
+            System.out.println("Funcionário não encontrado.");
+        }
     }
 
+    // Remover funcionário
+    public static void removerFuncionario(Scanner sc, List<Funcionario> funcionarioList) {
+        System.out.println("=== Remover Funcionário ===");
+        System.out.print("Informe o nome do funcionário a remover: ");
+        String nomeRemover = sc.nextLine();
+        boolean encontrado = false;
+
+        Iterator<Funcionario> iterator = funcionarioList.iterator();
+        while (iterator.hasNext()) {
+            Funcionario funcionario = iterator.next();
+            if (funcionario.getNome().equalsIgnoreCase(nomeRemover)) {
+                iterator.remove();
+                System.out.println("Funcionário removido com sucesso.");
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("Funcionário não encontrado.");
+        }
+    }
 }
